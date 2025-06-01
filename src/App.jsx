c3be38b905e880d749d34082c4d40b6706ec7a56
Manish <manishshekhawat756@gmail.com>
2025-06-01 12:47:13 +0530

    m

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import { useRef, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [time, setTime] = useState(0);
+  const counter = useRef(0);
+
+  let resetInterval = useRef(null);
+  const startTime = () => {
+
+    if(resetInterval.current) return;
+    resetInterval.current = setInterval(() => {
+      setTime(counter.current);
+      counter.current += 0.01;
+    }, 10);
+  };
+
+  const resetTime = () => {
+    clearInterval(resetInterval.current);
+    resetInterval.current=null;
+    setTime(0);
+    counter.current = 0;
+
+  };
+
+  const stopTime = () => {
+    clearInterval(resetInterval.current);
+    resetInterval.current=null;
+
+  };
+
+  
+
+  return (
+    <>
+      <h1>Stopwatch</h1>
+      <p>Time: {time.toFixed(2)}</p>
+
+      {
+        resetInterval.current ? (<button type="button" onClick={stopTime}>
+        Stop
+      </button>):(<button type="button" onClick={startTime}>
+        Start
+      </button>)
+      }
+      
+      
+
+      <button type="button" onClick={resetTime}>
+        Reset
+      </button>
+    </>
+  );
+}
+
+export default App;
